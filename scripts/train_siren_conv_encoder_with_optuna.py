f08f4ae643af9f2c158309298730060f4b7ef084
Sergey <ivashchenko_serezha@mail.ru>
2026-04-22 10:14:21 +0300

    Hot fix

--- scripts/train_siren_conv_encoder_with_optuna.py
+++ scripts/train_siren_conv_encoder_with_optuna.py
@@ -1,16 +1,18 @@
 import optuna
 import torch
 
+from configs import *
 from data.utils.loader import get_data
+from models.siren_conv_encoder import get_model
 from training.train_siren_conv_encoder import train
 
 
-def train_model(config, model):
+def train_model(config):
 
     def objective(trial):
 
         config.lr = trial.suggest_float("lr", 1e-5, 1e-2)
-        config.weight_decay = trial.suggest_float("lr", 0, 1e-1)
+        config.weight_decay = trial.suggest_float("weight_decay", 0, 1e-1)
         config.c_hyperparam = [1, 1, trial.suggest_float("c3", 0, 1e-2)]
 
         config.siren_num_hidden_layers = trial.suggest_int("siren_num_layers", 1, 5)
@@ -26,6 +28,16 @@ def train_model(config, model):
         print("Current params")
         for key_, value_ in trial.params.items():
             print("    {}: {}".format(key_, value_))
+
+        model = get_model(
+            out_channels=config.out_channels,
+            embedding_size=config.embedding_size,
+            siren_num_hidden_layers=config.siren_num_hidden_layers,
+            siren_hidden_size=config.siren_hidden_size,
+            conv_hidden_layers=config.conv_hidden_layers,
+            conv_out_channels=config.conv_out_channels,
+            conv_kernel_size=config.conv_kernel_size
+        )
 
         optimizer = torch.optim.Adam(model.parameters(), lr=config.lr, weight_decay=config.weight_decay)
         timestamps, data, data_derivatives, data_noised = get_data(config.data_path)
@@ -56,4 +68,6 @@ def train_model(config, model):
     for key, value in best_trial.params.items():
         print("    {}: {}".format(key, value))
 
-    return model
+
+train_config = Config20()
+train_model(train_config)

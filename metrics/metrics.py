123644d5aace9fad7a01385e0a5e6061b6cba590
Sergey <ivashchenko_serezha@mail.ru>
2026-04-28 17:33:29 +0300

    Fix Metrics Namings

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def metric(input_x, output_y, device='cpu') -> float:
-    x = torch.as_tensor(input_x, dtype=torch.float32).to(device)
-    y = torch.as_tensor(output_y, dtype=torch.float32).to(device)
+def metric(ground_truth, predicted, device='cpu') -> float:
+    x = torch.as_tensor(ground_truth, dtype=torch.float32).to(device)
+    y = torch.as_tensor(predicted, dtype=torch.float32).to(device)
     return (torch.linalg.norm(x - y) / torch.linalg.norm(x)).item()
